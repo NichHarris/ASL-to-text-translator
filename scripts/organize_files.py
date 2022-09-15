72a28ed295bf3a86c42537afdffed1183cc3382c
NichHarris <harris.nicholas1998@gmail.com>
2022-09-15 15:01:37 -0400

    Split long files

--- scripts/organize_files.py
+++ scripts/organize_files.py
@@ -7,6 +7,8 @@ LOGS = '../logs'
 FILES_DIRECTORY = '../files'
 VIDEO_FILE_NAME = 'raw_videos_mp4'
 OUTPUT_DIR = '../files/organized_files'
+COMPOUND_VIDEO_DIR = '../files/extra_videos'
+MAX_SIZE = 2000
 
 def init_log():
     if not os.path.exists(LOGS):
@@ -39,7 +41,6 @@ def retrieve_files(filename: str):
     return
 
 def organize_videos(filename: str):
-
     video_list = os.listdir(filename)
     video_list = sorted(video_list)
     last_word = ''
@@ -50,6 +51,7 @@ def organize_videos(filename: str):
 
     try:
         os.mkdir(OUTPUT_DIR)
+        os.mkdir(COMPOUND_VIDEO_DIR)
     except Exception as e:
         logging.error(f'ERROR -Unable to create directory {OUTPUT_DIR}: {str(e)}')
         return
@@ -71,7 +73,11 @@ def organize_videos(filename: str):
             os.mkdir(add_path)
         
         # add it to the directory
-        os.rename(f'{FILES_DIRECTORY}/{filename}/{video}', f'{add_path}/{video_name}_{word_occurence_count}.mp4')
+        size = os.stat(f'{FILES_DIRECTORY}/{filename}/{video}').st_size/1024
+        if size < MAX_SIZE:
+            os.rename(f'{FILES_DIRECTORY}/{filename}/{video}', f'{add_path}/{video_name}_{word_occurence_count}.mp4')
+        else:
+            os.rename(f'{FILES_DIRECTORY}/{filename}/{video}', f'{COMPOUND_VIDEO_DIR}/{video_name}_{word_occurence_count}.mp4')
         word_occurence_count += 1
     
     logging.info(f'Processing finished...\nVideos processed: {video_count}\nUnique words: {dir_count}')
