aed1cd85cbd9137434eb5b4aaedf5cc126d3d09f
Nicholas Kawwas <nicholas.b.kawwas@gmail.com>
2023-01-05 17:06:56 -0500

    Fixed matrix augmentation script to save each video as an array of tensors, with small rotations in all directions

--- matrix_augmentation.py
+++ matrix_augmentation.py
@@ -1,63 +1,106 @@
 import numpy as np
+import torch
 from math import pi, sin, cos
+from os import listdir, makedirs, path
 
-'''
-# Rotate point using phase
-def rotate_point(curr_pt, next_pt, phase):
-    return curr_pt*sin(phase) + next_pt*cos(phase)
-
-# Rotate on x-axis 
-# https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Rotation_of_coordinates.svg/320px-Rotation_of_coordinates.svg.png
-def rotate_x(x, y, z, phase):
-    y_new = rotate_point(y, -1*z, phase)
-    z_new = rotate_point(z, y, phase)
-
-    return x, y_new, z_new
-
-
-# Rotate on y-axis
-def rotate_y(x, y, z, phase):
-    x_new = rotate_point(x, z, phase)
-    z_new = rotate_point(z, -1*x, phase)
-
-    return x_new, y, z_new
-
-# Rotate on z-axis
-def rotate_z(x, y, z, phase):
-    x_new = rotate_point(x, -1*y, phase)
-    y_new = rotate_point(y, x, phase)
-
-    return x_new, y_new, z
-'''
-
-
-# Improved method for rotation
-degree = 1
+PREPROCESS_PATH = "./preprocess"
+POSE_START = 21*3*2
 
 # Convert degree to radians
-theta = degree*pi/180
+degrees = [-1, -2, -5, 1, 2, 5]
 
-# Define rotation matrix constants
-z_rotation_matrix = np.array([ 
-        [ cos(theta), -sin(theta), 0 ], 
-        [ sin(theta), cos(theta), 0 ], 
-        [ 0, 0, 1 ]
-    ])
+def apply_rotation(coordinates, rotation_matrix):
+    # rotation_matrix * [[x], [y], [z]]
+    # [x, y, z] * rotation_matrix
+    return torch.matmul(coordinates, rotation_matrix)
 
-x_rotation_matrix = np.array([ 
-        [ 1, 0, 0 ], 
-        [ 0, cos(theta), -sin(theta) ], 
-        [ 0, sin(theta), cos(theta) ]
-    ])
+def collect_aug_frame(hands, pose, rotation_matrix):
+    # Augmented frames
+    aug_hands, aug_pose = hands.detach().clone(), pose.detach().clone()
 
-y_rotation_matrix = np.array([ 
-        [ cos(theta), 0, sin(theta) ], 
-        [ 0, 1, 0], 
-        [ -sin(theta), 0, cos(theta)]
-    ])
+    # Perform augmentation
+    curr_ind = 0
+    h_iter = iter(aug_hands)
+    for x, y, z in zip(h_iter, h_iter, h_iter):
+        # aug_hands = torch.cat((aug_hands, apply_rotation(torch.tensor([x, y, z]), rotation_matrix)))
+        aug_hands[curr_ind:curr_ind + 3] = apply_rotation(torch.tensor([x, y, z]), rotation_matrix)
+        curr_ind += 3
+    
+    curr_ind = 0
+    p_iter = iter(aug_pose)
+    for x, y, z, v in zip(p_iter, p_iter, p_iter, p_iter):
+        # aug_pose = torch.cat((aug_pose, apply_rotation(torch.tensor([x, y, z]), rotation_matrix), torch.tensor([v])))
+        aug_pose[curr_ind:curr_ind + 3] = apply_rotation(torch.tensor([x, y, z]), rotation_matrix)
+        aug_pose[curr_ind + 3] = v
+        curr_ind += 4
 
-# Perform matrix multiplication
+    return torch.cat((aug_hands, aug_pose))
 
-# TODO:
-# rotation_matrix * [[x], [y], [z]]
-# [x, y, z] * rotation_matrix
+def main():
+    # Perform all degree rotations in each axis
+    for i, degree in enumerate(degrees):
+        print(f"\n--- Starting {degree} matrix rotation ---")
+        theta = degree*pi/180
+
+        # Define rotation matrix constants
+        z_rotation_matrix = torch.tensor([ 
+                [ cos(theta), -sin(theta), 0 ], 
+                [ sin(theta), cos(theta), 0 ], 
+                [ 0, 0, 1 ]
+            ])
+
+        x_rotation_matrix = torch.tensor([ 
+                [ 1, 0, 0 ], 
+                [ 0, cos(theta), -sin(theta) ], 
+                [ 0, sin(theta), cos(theta) ]
+            ])
+
+        y_rotation_matrix = torch.tensor([ 
+                [ cos(theta), 0, sin(theta) ], 
+                [ 0, 1, 0], 
+                [ -sin(theta), 0, cos(theta)]
+            ])
+
+        # Get all actions/gestures names
+        actions = listdir(PREPROCESS_PATH)
+        for action in actions:
+            print(f"\n--- Starting {action} matrix augmentation ---")
+            preprocess_folder = f"{PREPROCESS_PATH}/{action}"
+
+            # Get all filenames for preprocessing each
+            videos = listdir(preprocess_folder)
+            for video in videos: 
+                # Skip already augmented files
+                if video.startswith("mat_"):
+                    continue
+
+                # Load data instance
+                frames = torch.load(f"{preprocess_folder}/{video}")
+                
+                # Store augmentated frames
+                x_frames = []
+                y_frames = []
+                z_frames = []
+                
+                # Iterate frame by frame
+                for frame in frames:
+                    # Split tensor into tracked parts
+                    hands = frame[0 : POSE_START]
+                    pose = frame[POSE_START:]
+
+                    # Augment frame using rotation matrices
+                    x_aug_frame = collect_aug_frame(hands, pose, x_rotation_matrix)
+                    x_frames.append(x_aug_frame)
+
+                    y_aug_frame = collect_aug_frame(hands, pose, y_rotation_matrix)
+                    y_frames.append(y_aug_frame)
+
+                    z_aug_frame = collect_aug_frame(hands, pose, z_rotation_matrix)                    
+                    z_frames.append(z_aug_frame)
+
+                # Save augmented frames as torch files
+                # torch.save(x_frames, f'{preprocess_folder}/mat_x_{i}.pt')
+                # torch.save(y_frames, f'{preprocess_folder}/mat_y_{i}.pt')
+                # torch.save(z_frames, f'{preprocess_folder}/mat_z_{i}.pt')
+    
+main()
