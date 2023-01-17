a2d3846d01a6590a5ee14a03658067e334ae5093
Nicholas Kawwas <nicholas.b.kawwas@gmail.com>
2023-01-17 13:22:49 -0500

    Making progress on augmentation

--- matrix_augmentation.py
+++ matrix_augmentation.py
@@ -104,19 +104,107 @@ def get_rotated_frames(video, rot1_matrix, rot2_matrix):
     
     return rot1_frames, rot2_frames
 
+def load_sign_video(preprocess_folder, video):
+    return torch.load(f"{preprocess_folder}/{video}")
+
+
 def save_rotated_frames(vid_prefix, rot1_frames, rot1_angle, rot2_frames, rot2_angle): 
-    torch.save(rot1_frames, f'{preprocess_folder}/{vid_prefix}_{rot1_angle}.pt')
-    torch.save(rot2_frames, f'{preprocess_folder}/{vid_prefix}_{rot2_angle}.pt')
+    print(f'{preprocess_folder}/{vid_prefix}_{rot1_angle}.pt')
+    print(f'{preprocess_folder}/{vid_prefix}_{rot2_angle}.pt')
+    # torch.save(rot1_frames, f'{preprocess_folder}/{vid_prefix}_{rot1_angle}.pt')
+    # torch.save(rot2_frames, f'{preprocess_folder}/{vid_prefix}_{rot2_angle}.pt')
 
 def main():
     start_time = time.time()
 
     # Multiple Translation (on Original videos - 36x)
     shift_pcs = [-0.015, -0.01, -0.005, 0.005, 0.01, 0.015]
-
     ct = torch.tensor([1])
-    frames = torch.load(f"{PREPROCESS_PATH}/bye/bye_0.pt")
 
+    # Get all actions/gestures names
+    actions = listdir(PREPROCESS_PATH)
+    for action in actions:
+        print(f"\n--- Starting {action} matrix augmentation ---")
+        preprocess_folder = f"{PREPROCESS_PATH}/{action}"
+
+        aug_count = 0
+
+        # Get all filenames for preprocessing each
+        print(f'\n-- Performing Simple Translations --')
+        videos = listdir(preprocess_folder)
+        for video in videos:   
+            video_prefix = video.split('.pt')[0]  # Adjust '.' to '.pt'  
+            frames = load_sign_video(preprocess_folder, video)
+
+            # Translation       
+            for dx in shift_pcs:
+                for dy in shift_pcs:
+                    trans_matrix = get_translation_matrix(dx, dy)
+                    print(f'Shift x by: {dx}%, Shift y by: {dy}%')
+
+                    for frame in frames:
+                        hands, pose = extract_hands_pose(frame)
+
+                        # Augmented frames
+                        aug_hands, aug_pose = hands.detach().clone(), pose.detach().clone()
+
+                        # Perform augmentation
+                        curr_ind = 0
+                        for i in range(42):
+                            curr_ind = i*3
+
+                            # Skip undetected hands
+                            if aug_hands[curr_ind] == 0 and aug_hands[curr_ind + 1] == 0 and aug_hands[curr_ind + 2] == 0:
+                                continue
+
+                            prep_hands = torch.cat([ aug_hands[curr_ind:curr_ind + 3], ct ])
+                            aug_hands[curr_ind:curr_ind + 3] = torch.matmul(prep_hands, trans_matrix)[0:3]
+
+                        for i in range(25):
+                            curr_ind = i*4
+                            prep_pose = torch.cat([ aug_pose[curr_ind:curr_ind + 3], ct ])
+                            aug_pose[curr_ind:curr_ind + 3] = torch.matmul(prep_pose, trans_matrix)[0:3]
+
+                    print(f'mat_trans_{aug_count}_x={dx}_y={dy}.pt')
+                    aug_count += 1
+    
+        print(f'\n-- Performing Single Rotations --')
+        videos = listdir(preprocess_folder)
+        for video in videos:
+            video_prefix = video.split('.pt')[0]  # Adjust '.' to '.pt'  
+
+            # Single Rotation
+            degrees = [-3, -2, -1, 1, 2, 3]
+            for degree in degrees:
+                # Check if video doesn't start with mat
+                if not video_prefix.startswith('mat'):
+                    print(f"AUG 1 - {video_prefix}")
+                    continue
+                print(f'mat_rot_{aug_count}_x={degree}.pt')
+                print(f'mat_rot_{aug_count}_y={degree}.pt')
+                print(f'mat_rot_{aug_count}_z={degree}.pt')
+
+                aug_count += 3
+
+        print(f'\n-- Performing Multiple Rotations --')
+        videos = listdir(preprocess_folder)
+        for video in videos:   
+            video_prefix = video.split('.pt')[0]  # Adjust '.' to '.pt'  
+      
+            # Multiple Rotation
+            degrees = [ -2, -1, 1, 2 ]
+            for degree in degrees:
+                if not video_prefix.startswith('mat'):
+                    print(f"AUG 2 - {video_prefix}")
+                    continue
+                # Check if video doesn't start with mat
+                print(f'{video_prefix}_rot_x={degree}.pt')
+                print(f'{video_prefix}_rot_y={degree}.pt')
+                print(f'{video_prefix}_rot_z={degree}.pt')
+        
+        break
+
+    
     for dx in shift_pcs:
         for dy in shift_pcs:
             trans_matrix = get_translation_matrix(dx, dy)
@@ -129,7 +217,6 @@ def main():
                 aug_hands, aug_pose = hands.detach().clone(), pose.detach().clone()
 
                 # Perform augmentation
-                # print(torch.cat((aug_hands, aug_pose)))
                 curr_ind = 0
                 for i in range(42):
                     curr_ind = i*3
@@ -146,13 +233,9 @@ def main():
                     prep_pose = torch.cat([ aug_pose[curr_ind:curr_ind + 3], ct ])
                     aug_pose[curr_ind:curr_ind + 3] = torch.matmul(prep_pose, trans_matrix)[0:3]
 
-                # print(torch.cat((aug_hands, aug_pose)))
-                # for i in range(25):
-                #     curr_ind = i*4
-                #     print(aug_pose[curr_ind:curr_ind + 3] / pose[curr_ind:curr_ind + 3])
 
     # Single Rotation (on Orig + Translated - 6x)
-    degrees = [-5, -2, -1, 1, 2, 5]
+    degrees = [-3, -2, -1, 1, 2, 3]
 
     # Perform all degree rotations in each axis
     for i, degree in enumerate(degrees):
@@ -190,8 +273,7 @@ def main():
                 x_frames = []
                 y_frames = []
                 z_frames = []
-
-                
+           
                 # Iterate frame by frame
                 for frame in frames:
                     # Split tensor into tracked parts
@@ -207,79 +289,67 @@ def main():
 
                     z_aug_frame = collect_aug_frame(hands, pose, z_rotation_matrix)                    
                     z_frames.append(z_aug_frame)
-
-                    # TODO: Currently only applying same rotation angle for 2 rotations
-                    # - Make rotations on all angles
-                    # Double augmment frame in 2 diff rotations
-                    # x_hands, x_pose = x_aug_frame[0 : POSE_START], x_aug_frame[POSE_START:]
-                    # xy_aug_frame = collect_aug_frame(x_hands, x_pose, y_rotation_matrix)
-                    # xy_frames.append(xy_aug_frame)
-
-                    # y_hands, y_pose = y_aug_frame[0 : POSE_START], y_aug_frame[POSE_START:]
-                    # yz_aug_frame = collect_aug_frame(y_hands, y_pose, z_rotation_matrix)
-                    # yz_frames.append(yz_aug_frame)
-
-                    # z_hands, z_pose = z_aug_frame[0 : POSE_START], z_aug_frame[POSE_START:]
-                    # zx_aug_frame = collect_aug_frame(z_hands, z_pose, x_rotation_matrix)                    
-                    # zx_frames.append(zx_aug_frame)
                 
                 # # Save augmented frames as torch files
                 # torch.save(x_frames, f'{preprocess_folder}/mat_x_{i}.pt') # x={degree}
                 # torch.save(y_frames, f'{preprocess_folder}/mat_y_{i}.pt')
                 # torch.save(z_frames, f'{preprocess_folder}/mat_z_{i}.pt')
-
-                # # Save double augmented frames as torch files
-                # torch.save(xy_frames, f'{preprocess_folder}/mat_xy_{i}.pt')
-                # torch.save(yz_frames, f'{preprocess_folder}/mat_yz_{i}.pt')
-                # torch.save(zx_frames, f'{preprocess_folder}/mat_zx_{i}.pt')
     
-    # Multiple Rotation (on Single Rotation - 16x)
+    # Multiple Rotation (on Single Rotation - 4x)
     small_degrees = [-2, -1, 1, 2]
     for d1 in small_degrees:
-        for d2 in small_degrees:
-            t1 = d1*pi/180
-            t2 = d2*pi/180
+        t1 = d1*pi/180
 
-            vid_prefix = video.split(".")[0]
-            if 'x=' in vid_prefix:
-                y_rotation_matrix = get_rotation_matrix_y(t1)
-                z_rotation_matrix = get_rotation_matrix_z(t2)
+        vid_prefix = video.split(".")[0]
+        if 'x=' in vid_prefix:
+            y_rotation_matrix = get_rotation_matrix_y(t1)
+            z_rotation_matrix = get_rotation_matrix_z(t1)
 
-                y_aug_frames, z_aug_frames = get_rotated_frames(video, y_rotation_matrix, z_rotation_matrix)
-                save_rotated_frames(vid_prefix, y_aug_frames, f'y={d1}', z_aug_frames, f'z={d2}')
-            elif 'y=' in vid_prefix:
-                x_rotation_matrix = get_rotation_matrix_x(t1)
-                z_rotation_matrix = get_rotation_matrix_z(t2)
+            y_aug_frames, z_aug_frames = get_rotated_frames(video, y_rotation_matrix, z_rotation_matrix)
+            save_rotated_frames(vid_prefix, y_aug_frames, f'y={d1}', z_aug_frames, f'z={d1}')
+        elif 'y=' in vid_prefix:
+            x_rotation_matrix = get_rotation_matrix_x(t1)
+            z_rotation_matrix = get_rotation_matrix_z(t1)
 
-                x_aug_frames, z_aug_frames = get_rotated_frames(video, x_rotation_matrix, z_rotation_matrix)
-                save_rotated_frames(vid_prefix, x_aug_frames, f'x={d1}', z_aug_frames, f'z={d2}')
-            elif 'z=' in vid_prefix:
-                x_rotation_matrix = get_rotation_matrix_x(t1)
-                y_rotation_matrix = get_rotation_matrix_y(t2)
+            x_aug_frames, z_aug_frames = get_rotated_frames(video, x_rotation_matrix, z_rotation_matrix)
+            save_rotated_frames(vid_prefix, x_aug_frames, f'x={d1}', z_aug_frames, f'z={d1}')
+        elif 'z=' in vid_prefix:
+            x_rotation_matrix = get_rotation_matrix_x(t1)
+            y_rotation_matrix = get_rotation_matrix_y(t1)
 
-                x_aug_frames, y_aug_frames = get_rotated_frames(video, x_rotation_matrix, y_rotation_matrix)
-                save_rotated_frames(vid_prefix, x_aug_frames, f'x={d1}', y_aug_frames, f'y={d2}')
-            else:
-                print(vid_prefix)
-                print('Skipping this video')
+            x_aug_frames, y_aug_frames = get_rotated_frames(video, x_rotation_matrix, y_rotation_matrix)
+            save_rotated_frames(vid_prefix, x_aug_frames, f'x={d1}', y_aug_frames, f'y={d1}')
+        else:
+            print(vid_prefix)
+            print('Skipping this video')
     
+
     end_time = time.time()
     print("\nTotal Matrix Augmentation Time (s): ", end_time - start_time)
 
-# main()
+main()
+
+# x_rotation_matrix = get_rotation_matrix_x(-3*pi/180)
+# frames = torch.load(f"{PREPROCESS_PATH}/bye/bye_0.pt")
+
+# # Store augmentated frames
+# x_frames = []
+
+# # Iterate frame by frame
+# for frame in frames:
+#     # Split tensor into tracked parts
+#     hands = frame[0 : POSE_START]
+#     pose = frame[POSE_START:]
+    
+#     x_aug_frame = collect_aug_frame(hands, pose, x_rotation_matrix)
+#     x_frames.append(x_aug_frame)
+
+#     for i in range(25):
+#         curr_ind = 126 + i*4
+#         print(frame[curr_ind:curr_ind+3] / x_aug_frame[curr_ind:curr_ind+3])
 
 
 
-        # trans_frame = []
-        # for frame in frames:
-        #     # Split tensor into tracked parts
-        #     hands = frame[0 : POSE_START]
-        #     pose = frame[POSE_START:]
-
-        #     # Augment frame using translation matrix
-        #     xy_trans_frame = collect_aug_frame(hands, pose, xy_shift_matrix)
-
-        # torch.save(xy_trans_frames, f'{preprocess_folder}/mat_trans_xy_{i}.pt')
 
 
 '''
