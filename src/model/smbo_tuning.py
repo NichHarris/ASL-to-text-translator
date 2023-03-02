7f9a337555242aabb8e8f66da121c38938b9b669
Nicholas Kawwas <nicholas.b.kawwas@gmail.com>
2023-03-02 11:10:43 -0800

    Integrated ray tune with optuna for hyperparam opt

--- src/model/smbo_tuning.py
+++ src/model/smbo_tuning.py
@@ -32,7 +32,10 @@ Provides importance of hyperparameters
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torchvision 
+from ray import tune, air
+from ray.air import session
+from ray.tune.search import ConcurrencyLimiter
+from ray.tune.search.bayesopt import BayesOptSearch
 
 import os
 
@@ -40,52 +43,53 @@ from dataset_loader import SignLanguageGestureDataset
 from asl_model import AslNeuralNetwork
 
 from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
 
-import optuna
-from optuna.samplers import TPESampler
-# pip3.10 install optuna optuna-dashboard
-# pip3.10 install mysql-connector-python
+# pip3.10 install optuna optuna-dashboard "ray[default]" "ray[air]" "ray[tune]" bayesian-optimization
 
 # TODO: Determine batch size and num epochs
 # Optimal batch size: 64 (Must be divisible by 8) -> 512
-NUM_EPOCHS = 60
+NUM_EPOCHS = 120
 BATCH_SIZE = 64
-VALID_BATCH_SIZE = 8
-
-MODEL_PATH = "../../smbo"
+TEST_BATCH_SIZE = 8
 
 # -- Constants -- #
-DATASET_PATH = '../../inputs/dataset_no_aug_no_vis'
+DATASET_PATH = os.path.abspath('../../inputs/dataset_no_aug')
 DATASET_FILES = os.listdir(DATASET_PATH)
 DATASET_SIZE = len(DATASET_FILES)
 
-VALID_SET_PATH= '../../processed_tests/joint_no_vis'
-VALID_SET_FILES = os.listdir(VALID_SET_PATH)
-VALID_SET_SIZE = len(VALID_SET_FILES)
-print(DATASET_SIZE, VALID_SET_SIZE)
+TEST_SET_PATH= os.path.abspath('../../processed_tests/joint')
+TEST_SET_FILES = os.listdir(TEST_SET_PATH)
+TEST_SET_SIZE = len(TEST_SET_FILES)
 
 # Define signed words/action classes 
 word_dict = {'bad': 0, 'bye': 1, 'easy': 2, 'good': 3, 'happy': 4, 'hello': 5, 'like': 6, 'me': 7, 'meet': 8, 'more': 9, 'no': 10, 'please': 11, 'sad': 12, 'she': 13, 'sorry': 14, 'thank you': 15, 'want': 16, 'why': 17, 'yes': 18, 'you': 19}
 
 # -- Load dataset -- #
-train_set = SignLanguageGestureDataset(DATASET_PATH, DATASET_FILES, word_dict)
-train_loader = DataLoader(dataset=train_set, batch_size=BATCH_SIZE, shuffle=True)
+TRAIN_SPLIT = int(DATASET_SIZE * 0.9)
+VALID_SPLIT = DATASET_SIZE - TRAIN_SPLIT
 
-valid_set = SignLanguageGestureDataset(VALID_SET_PATH, VALID_SET_FILES, word_dict)
-valid_loader = DataLoader(dataset=valid_set, batch_size=VALID_BATCH_SIZE, shuffle=True)
+dataset = SignLanguageGestureDataset(DATASET_PATH, DATASET_FILES, word_dict)
+
+# Split into training, validation and testing sets
+train_split, valid_split = torch.utils.data.random_split(dataset, [TRAIN_SPLIT, VALID_SPLIT])
+
+train_loader = DataLoader(dataset=train_split, batch_size=BATCH_SIZE, shuffle=True)
+valid_loader = DataLoader(dataset=valid_split, batch_size=BATCH_SIZE, shuffle=True)
+
+test_set = SignLanguageGestureDataset(TEST_SET_PATH, TEST_SET_FILES, word_dict)
+test_loader = DataLoader(dataset=test_set, batch_size=TEST_BATCH_SIZE, shuffle=True)
 
 train_size = len(train_loader)
 valid_size = len(valid_loader)
 
 no_train_loss_items = int(train_size/5)
-no_valid_loss_items = int(valid_size/5)
+no_valid_loss_items = int(valid_size/3)
 
-# -- Train Model -- #
-def train_model(trial, params):
+def objective(params):
     # Create model
     model = AslNeuralNetwork(lstm_hidden_size=params['lstm_hidden_size'], num_lstm_layers=params['num_lstm_layers'], dropout_rate=params['dropout'])
     print(f"{params['num_lstm_layers']} x {params['lstm_hidden_size']} biLSTM w/ lr {params['learning_rate']} + dr {params['dropout']}")
+    model.to(model.device)
 
     # Define loss and optimizer
     criterion = nn.CrossEntropyLoss()
@@ -94,6 +98,7 @@ def train_model(trial, params):
     patience = 5
     patience_counter = 0
     best_epoch_loss = 100
+    best_epoch_accuracy = 0
     for epoch in range(NUM_EPOCHS):
         print(f'--- Starting Epoch #{epoch} ---')
 
@@ -116,10 +121,10 @@ def train_model(trial, params):
 
             # Compute training loss
             train_total_loss += loss.item()
-            mini_batch_loss += loss.item()
-            if (i + 1) % no_train_loss_items == 0:
-                print(f"Training Loss - {i + 1}/{train_size}: {mini_batch_loss/no_train_loss_items}")
-                mini_batch_loss = 0
+            # mini_batch_loss += loss.item()
+            # if (i + 1) % no_train_loss_items == 0:
+            #     print(f"Training Loss - {i + 1}/{train_size}: {mini_batch_loss/no_train_loss_items}")
+            #     mini_batch_loss = 0
 
         # -- Validation -- #
         mini_batch_loss = 0
@@ -135,11 +140,36 @@ def train_model(trial, params):
             
             # Compute validation loss
             valid_total_loss += loss.item()
-            mini_batch_loss += loss.item()
-            if (i + 1) % no_valid_loss_items == 0:
-                print(f'Validation Loss - {i + 1}/{valid_size}: {mini_batch_loss/no_valid_loss_items}')
-                mini_batch_loss = 0
+            # mini_batch_loss += loss.item()
+            # if (i + 1) % no_valid_loss_items == 0:
+            #     print(f'Validation Loss - {i + 1}/{valid_size}: {mini_batch_loss/no_valid_loss_items}')
+            #     mini_batch_loss = 0
         
+        # -- Quick Test -- #
+        true_count = 0
+        test_count = 0
+        for i, (keypoints, labels) in enumerate(test_loader):  
+            # Use GPU for model validation computations  
+            keypoints = keypoints.to(model.device)
+            labels = labels.to(model.device)
+            
+            # Forward pass
+            output = model(keypoints)
+
+            # Obtain max prob value and class index
+            _, predicted = torch.max(output.data, 1)
+            
+            true_count += (predicted == labels).sum().item()
+            test_count += labels.size(0)
+            
+        model_accuracy = (true_count / test_count) * 100
+        print(f'Model Accuracy: {model_accuracy:.2f}%')
+
+        if best_epoch_accuracy < model_accuracy:
+            # torch.save(model.state_dict(), f'{MODEL_PATH}/{trial.number}/asl_model_{trial.number}_acc={model_accuracy:.2f}_epo={epoch}.pth')
+            best_epoch_accuracy = model_accuracy
+            print(f'->Model Saved w/ Best Accuracy<-')
+
         # Early Stopping + Save model with improved validation loss
         train_epoch_loss = train_total_loss/train_size
         valid_epoch_loss = valid_total_loss/valid_size
@@ -147,63 +177,56 @@ def train_model(trial, params):
             best_epoch_loss = valid_epoch_loss
 
             patience_counter = 0
-            torch.save(model.state_dict(), f'{MODEL_PATH}/{trial.number}/asl_model_{trial.number}_vl={valid_epoch_loss}.pth')
+            # torch.save(model.state_dict(), f'{MODEL_PATH}/{trial.number}/asl_model_{trial.number}_vl.pth')
             print(f'Model Saved - Train={train_epoch_loss}  Valid={valid_epoch_loss}')
         else:
             patience_counter += 1
 
             if patience_counter > patience:
                 print(f'Early Stopping - Train={train_epoch_loss}  Valid={valid_epoch_loss}')
-                return best_epoch_loss
+                break
         
-        # -- Pruning Strategy -- 
-        # Evaluate trial based on performance relative to time/depth
-        # to terminate or continue trial
-        trial.report(best_epoch_loss, epoch)
-        if trial.should_prune(): 
-            raise optuna.exceptions.TrialPruned()
-
-    return best_epoch_loss
+        # session.report({"model_accuracy": model_accuracy})
+        session.report({'validation_loss': valid_epoch_loss})
 
 
-def objective(trial):
-    params = {
-        'num_lstm_layers': trial.suggest_int('num_lstm_layers', 3, 6), # 8
-        'lstm_hidden_size': trial.suggest_int('lstm_hidden_size', 128, 256), #512
-        'learning_rate':  trial.suggest_float('learning_rate', 1e-4, 1e-1),
-        'batch_size': trial.suggest_int('batch_size', 64, 1024),
-        'dropout': trial.suggest_float('dropout', 0.2, 0.5) 
+#utility_kwargs={"kind": "ucb", "kappa": 2.5, "xi": 0.0}
+# Bayesian Optimization Search with Gaussian Process and UCB (Upper confidence bound)
+algo = BayesOptSearch()
+algo = ConcurrencyLimiter(algo, max_concurrent=4)
+
+# Integrate Raytune with Optuna 
+tuner = tune.Tuner(
+    objective,
+    tune_config=tune.TuneConfig(
+        metric='validation_loss',#"model_accuracy", #
+        mode='min',#"max", #
+        search_alg=algo,
+        num_samples=4
+    ),
+    # run_config=air.RunConfig(
+    #     name="smbo_raytune_opt_min_v1",
+    # ),
+    param_space={
+        'num_lstm_layers': 4, #tune.choice([3, 4, 5, 6]),
+        'lstm_hidden_size': 128, #tune.choice([128, 256]),
+        'learning_rate':  tune.uniform(1e-4, 1e-1),
+        'dropout': tune.uniform(0.2, 0.8) 
     }
-    # TODO: Consider num fc nodes later
-    # 'fc_hidden_size': trial.suggest_int('fc_hidden_size', 64, 128), # 256
-
-    if not os.path.exists(f'{MODEL_PATH}/{trial.number}'):
-        os.makedirs(f'{MODEL_PATH}/{trial.number}')
-    
-    # Train with params
-    best_loss = train_model(trial, params)
-
-    return best_loss
-    
-# Create study minimizing validation loss or maximize accuracy
-# TODO: Look into how to setup mysql
-study = optuna.create_study(
-    direction='minimize',
-    study_name='top_20_words',
-    sampler=TPESampler()
-    # storage="mysql://root@127.0.0.1/optunadb", 
-    # load_if_exists= True,
-    # sampler= ..., pruner=...
 )
-study.optimize(objective, n_trials=100)
 
-trialx = study.best_trial
-print(trialx.values)
-print(trialx.params)
+res = tuner.fit()
+best_trial = res.get_best_result()
 
-# TODO: Integrate Raytune with Optuna 
+print("Best trial: ", best_trial)
+print("Best hyperparameters found were: ", best_trial.config)
 
-
-# brew install mysql
-# mysql -u root
-# CREATE DATABASE optunadb;
+'''
+trainable_with_resources = tune.with_resources(
+    trainable,
+    {
+        'gpu': 1,
+        'cpu': 4
+    }
+)
+'''
