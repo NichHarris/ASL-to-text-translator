282bb46b121fa666617fde0d7119e6c5dc859ff4
Nicholas Kawwas <nicholas.b.kawwas@gmail.com>
2023-03-12 03:10:54 -0400

    Fixed matrix augmentation script to perform 67 times faster and skip files already augmented

--- src/data/augment/video_augmentation.py
+++ src/data/augment/video_augmentation.py
@@ -1,119 +1,122 @@
 '''
-Perform video augmentation using rotation matrices and mediapipe coordinates
-- Augmentation performed only on original video and horizontally flipped video 
-    -> Ensure equal augmentation for left and right handed signers
-- Small angle rotations in x, y and z directions
-    -> Augmentation Factor: 3 (1, 2, 5 degrees) * 2 (+/- degrees) * 3 (x, y, z rotation) * 2 (original, horizontal aug) = 36 times per video!
+Data augmentation - alter existing training data to produce new artificial data
+-> prevents overfitting 
+-> increases training set size
+-> improves model performance
+-- Geometric transforms: crop, rotate and translate
+-- Color space transforms, kernel filters and noise injection (Not used due to using hand and pose estimation over images)
+-- Speed change, perspective skewing, elastic distortions, rotating, shearing, cropping, mirroring
+https://neptune.ai/blog/data-augmentation-in-python
 
-- Execution Time: 1m 30s for 5 signs
 
-- New Execution Time: 1933s ~ 32m for 5 signs
-- Newer Execution Time: 382s ~ 5m for 5 more signs
+Video augmentation of sign language videos
+- Create more videos using vidaug to produce a more robust deep learning model
+https://github.com/okankop/vidaug
 
-- Latest Execution Time: 1345 ~ 22m for 10 more signs
+- Horizontal flip on each augmented video (x2)
+    -> For both left and right hand signers
+Execution Time: 2m for 20 signs
+
+Old 
+- Spatial shear in X, Y by +/- 0.1 (+3)
+    -> For different viewing angles and signer body position wrt video camera
+- Random translate in x and y by +/- 50 pixels (+6)
+    -> For shifting height and width positions of signers wrt video camera
+- Crop video from each corner and center location to specific dimensions (5*3)
+    -> For shifting depth position of signers wrt video camera
+- Augmentation Factor: 1 (Horizontal flip) + [ 3 (Spatial shear) + 6 (Translation in x, y) + 5 (Crop pos in tl, tr, bl, br, c) * 3 (Crop factor of 0.925, 0.95, 0.975) ] * 2 (Horizontal flip on aug videos)
+    = 49 times per video!
+- Execution Time: 9m 12s for 5 gestures
 '''
 
-import os
-import math
+# pip3.10 install opencv-python scikit-image vidaug 
+
+import os 
+import cv2
 import time
-import torch
-import random
 import numpy as np
+from vidaug import augmentors as va
 
-PREPROCESS_PATH = "../../../inputs/interim"
+DATA_PATH = "../../../inputs/raw"
 
-MM_RESHAPE = (67, 3)
-NN_RESHAPE = (-1, )
+# Convert video to frames numpy array
+def store_frames(video):
+    # Open sign language video file
+    cap = cv2.VideoCapture(video)
 
-def apply_rotation(coordinates, rotation_matrix):
-    # [x, y, z] * rotation_matrix
-    return torch.mm(coordinates, rotation_matrix)
+    # Calculate frames per second
+    fps = cap.get(cv2.CAP_PROP_FPS)
 
-def collect_aug_frame(frames, rotation_matrix):
-    aug_frames = []
+    frames = []
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    # Perform augmentation
-    for i, frame in enumerate(frames):
-        # Detach frame to make changes and reshape for quick matrix multiplication
-        aug_frame = frame.detach().clone()
-        aug_frame = torch.reshape(aug_frame, MM_RESHAPE)
+        frames.append(frame)
 
-        aug_frame = apply_rotation(torch.reshape(aug_frame, MM_RESHAPE), rotation_matrix)
-        aug_frames.append(torch.reshape(aug_frame, NN_RESHAPE))
-    
-    return aug_frames
-   
+    # Close sign language video file
+    cap.release()
 
-# Return rotation matrix with theta
-def get_rotation_matrix_x(theta):
-    return torch.tensor([ 
-                [ 1, 0, 0 ],
-                [ 0, math.cos(theta), -1*math.sin(theta) ],
-                [ 0, math.sin(theta), math.cos(theta) ]
-            ])
+    # Convert frames to numpy
+    return np.array(frames), fps
 
-def get_rotation_matrix_y(theta):
-    return torch.tensor([ 
-                [ math.cos(theta), 0, math.sin(theta) ], 
-                [ 0, 1, 0], 
-                [ -1*math.sin(theta), 0, math.cos(theta)]
-            ])
 
-def get_rotation_matrix_z(theta):
-    return torch.tensor([ 
-                [ math.cos(theta), -1*math.sin(theta), 0 ], 
-                [ math.sin(theta), math.cos(theta), 0 ], 
-                [ 0, 0, 1 ]
-            ])
+# Pass augmentation sequence and file name
+def augment_video(seq, frames, curr_folder, vid_name, fps):
+    # Augment frames
+    aug_vid = seq(frames)
+    height, width, _ = aug_vid[0].shape
 
-def load_sign_video(preprocess_folder, video):
-    return torch.load(f"{preprocess_folder}/{video}")
+    # Save augmented data
+    # Define codec and video writer (Four character code for uniquely identifying file formats)
+    fourcc = 'mp4v'
+    video_writer = cv2.VideoWriter_fourcc(*fourcc)
+
+    # Save video 
+    out = cv2.VideoWriter(f"{DATA_PATH}/{curr_folder}/{vid_name}_flip.mp4", video_writer, fps, (width, height))
+    for frame in aug_vid:
+        out.write(frame)
+    out.release()
+
+    return aug_vid
 
 def main():
     start_time = time.time()
 
-    # Rotation degrees
-    degrees = [-3, -2, -1, 1, 2, 3]
+    # Define flip sequence
+    flip_seq = va.Sequential([ va.HorizontalFlip() ])
 
     # Get all actions/gestures names
-    actions = os.listdir(PREPROCESS_PATH)
-    num_actions = len(actions)
-    for i, action in enumerate(actions):
-        print(f"\n--- Starting {action} matrix augmentation ({i+1}/{num_actions}) ---")
-        preprocess_folder = f"{PREPROCESS_PATH}/{action}"
+    actions = os.listdir(DATA_PATH)
+    for action in actions:
+        print(f"\n-- Starting {action} augmentation --")
 
-        videos = os.listdir(preprocess_folder)
+        # Get all filenames for augmentating each
+        videos = os.listdir(f"{DATA_PATH}/{action}")
+
+        # Augment video by video
         for video in videos:
-            vid_prefix = video.split('.pt')[0]
-            sign_name, video_no, *_ = vid_prefix.split('_') 
-
-            # Skip file if already augmented 
-            if '_rot_' in vid_prefix:
+            # Skip flipping if flipped videos
+            if '_flip' in video:
                 continue
 
-            # Load data instance
-            frames = load_sign_video(preprocess_folder, video)
+            # Skip flipping if file already exists
+            vid_name, *_ = video.split('.mp4')
+            if os.path.isfile(f'{DATA_PATH}/{action}/{vid_name}_flip.mp4'):
+                print(f"- Skipping: {vid_name} -")
+                continue
             
-            # Perform rotation with multiple degrees
-            for degree in degrees:
-                theta = math.radians(degree)
+            print(f"- {vid_name} -")
 
-                # Define rotation matrix constants
-                x_rotation_matrix = get_rotation_matrix_x(theta)
-                y_rotation_matrix = get_rotation_matrix_y(theta)
-                z_rotation_matrix = get_rotation_matrix_z(theta)
+            # Capture frames per video
+            frames, fps = store_frames(f"{DATA_PATH}/{action}/{video}")
 
-                # Augment frames using rotation matrices
-                aug_x_frames = collect_aug_frame(frames, x_rotation_matrix)
-                aug_y_frames = collect_aug_frame(frames, y_rotation_matrix)
-                aug_z_frames = collect_aug_frame(frames, z_rotation_matrix)
-      
-                # Save augmented frames as torch files
-                # torch.save(aug_x_frames, f'{preprocess_folder}/{vid_prefix}_rot_x={degree}.pt')
-                # torch.save(aug_y_frames, f'{preprocess_folder}/{vid_prefix}_rot_y={degree}.pt')
-                # torch.save(aug_z_frames, f'{preprocess_folder}/{vid_prefix}_rot_z={degree}.pt')
-
+            # Augment video using frames
+            _, height, width, _ = frames.shape
+            augment_video(flip_seq, frames, action, vid_name, fps)
+        
     end_time = time.time()
-    print("\nTotal Matrix Augmentation Time (s): ", end_time - start_time)
+    print("\nTotal Video Augmentation Time (s): ", end_time - start_time)
 
 main()
